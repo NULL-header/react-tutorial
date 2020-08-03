12b8a6ebe7ca4074e457db451df2324d26b44f6b
NULL-header <58727175+NULL-header@users.noreply.github.com>
2020-08-03 17:27:11 +0900

    Initial commit

---
+++ webpack.config.ts
@@ -0,0 +1,45 @@
+import path from "path";
+import webpack from "webpack";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import ForkTsCheckerWebpackPlugin from "fork-ts-checker-webpack-plugin";
+import packageJSON from "./package.json";
+const webpackConfig = (env: {
+  production: any;
+  development: any;
+}): webpack.Configuration => ({
+  entry: "./src/index.tsx",
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
+  output: {
+    path: path.join(__dirname, "/public"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+        options: {
+          transpileOnly: true,
+        },
+        exclude: /public/,
+      },
+      {
+        test: /\.html$/,
+        loader: "html-loader",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: "./src/index.html" }),
+    new webpack.DefinePlugin({
+      "process.env.PRODUCTION": env.production || !env.development,
+      "process.env.NAME": JSON.stringify(packageJSON.name),
+      "process.env.VERSION": JSON.stringify(packageJSON.version),
+    }),
+    new ForkTsCheckerWebpackPlugin(),
+  ],
+});
+
+export default webpackConfig;
